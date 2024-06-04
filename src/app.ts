de19f0f99f939ad4237daed83afb905949e8d186
chrjl <73675826+chrjl@users.noreply.github.com>
2024-06-04 16:37:35 -0700

    feat: implement Swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import swaggerDocument from '../public/swagger.json';
 
 const app = express();
 
@@ -11,6 +13,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const options = {
+  swaggerOptions: {
+    tryItOutEnabled: true,
+  },
+};
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
 app.use('/public', express.static('public'));
 
 app.use('/', indexRouter);
