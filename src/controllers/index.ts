6155a283a01f3db3fde226bd7ce201ea030eeb4d
chrjl <73675826+chrjl@users.noreply.github.com>
2024-06-05 16:45:33 -0700

    feat(posts): GET /posts (getAllPostMetadata)

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,17 @@
 import db from '../configs/db';
 
+export function getAllPostMetadata() {
+  const query = `
+SELECT posts.id, title, description, created, modified, GROUP_CONCAT(keyword) AS keywords FROM posts
+  JOIN keywords ON keywords.post_id = posts.id
+  GROUP By posts.id 
+  `;
+
+  const result = db.prepare(query).all();
+  return result.map((row) => ({ ...row, keywords: row.keywords.split(',') }));
+}
+
+
+  return query.map((row) => ({ ...row, keywords: row.keywords.split(',') }));
+}
+
--- src/routes/api/posts.ts
+++ src/routes/api/posts.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { getAllPostMetadata } from '../../controllers';
 
 const router = express.Router();
 
-router.route('/').get((req, res) => res.send('hello from the posts route'));
+router.route('/').get((req, res) => {
+  res.json(getAllPostMetadata());
+});
 
 export default router;
