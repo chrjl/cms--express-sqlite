54ead0f965971fbe1bd93099d8edd56a36ed080b
chrjl <73675826+chrjl@users.noreply.github.com>
2024-06-06 08:20:07 -0700

    feat(frontend): reference app with GET functionality

---
+++ public/scripts/generate-allposts-form.js
@@ -0,0 +1,42 @@
+import { html, render } from 'https://esm.run/lit-html@1';
+
+// define HTML elements
+const allPostsFormElement = document.getElementById('allPostsFormElement');
+const allPostsSelectElement = document.getElementById('allPostsSelectElement');
+const postMetadataTextareaElement = document.getElementById(
+  'postMetadataTextareaElement'
+);
+const postKeywordsTextareaElement = document.getElementById(
+  'postKeywordsTextareaElement'
+);
+const postBodyTextareaElement = document.getElementById(
+  'postBodyTextareaElement'
+);
+
+// render <option> elements for all posts
+const res = await fetch('/api/posts');
+const allPosts = await res.json();
+
+const postOptionElementTemplates = allPosts.map(
+  ({ id, title }) => html`<option value=${id}>${title}</option>`
+);
+
+const allPostsTemplate = html`${postOptionElementTemplates}`;
+render(allPostsTemplate, document.getElementById('allPostsSelectElement'));
+
+// add event listener to form
+allPostsFormElement.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const postId = allPostsSelectElement.value;
+
+  const res = await fetch(`/api/posts/${postId}`);
+  const { body, ...metadata } = await res.json();
+
+  const res2 = await fetch(`/api/posts/${postId}/keywords`);
+  const keywords = await res2.json();
+
+  postMetadataTextareaElement.value = JSON.stringify(metadata, null, 2);
+  postKeywordsTextareaElement.value = JSON.stringify(keywords, null, 2);
+  postBodyTextareaElement.value = body;
+});
