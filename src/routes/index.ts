c2c04ec6de8c0358289e9c2b5a59d67df0f9bf4d
chrjl <73675826+chrjl@users.noreply.github.com>
2024-06-08 17:27:58 -0700

    feat: index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
 const router = express.Router();
 
+const links = /*html*/ `
+<p>
+  API root: <code>/api</code>
+  <br />
+  API routes: see documentation
+</p>
+<p>
+  API documentation
+  <ul>
+    <li><a href="/public/swagger.json">OpenAPI specification</a></li>
+    <li><a href="/api-docs">Swagger UI</a></li>
+  </ul>
+</p>
+<p><a href="/public/app.html">Reference front-end app</a></p>
+`;
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(links);
 });
 
 export default router;
